c47cf992bdc53acdb9c40996c4fd624e4056c666
Darantinao <jmjrjun024@gmail.com>
2026-01-20 14:14:21 +0800

    Filter

--- PizzaShop/src/Card.tsx
+++ PizzaShop/src/Card.tsx
@@ -20,27 +20,27 @@ function Card({ recipe }: CardProps) {
     
     return (
         <>
-            <div className='w-auto h-auto rounded-3xl text-2xl p-5 m-5 shadow-lg shadow-gray-700 hover:scale-105 duration-300'>
+            <div className='flex flex-col h-full w-auto rounded-3xl text-2xl p-5 m-4 shadow-lg shadow-gray-700 hover:scale-105 duration-300'>
                 <img src={recipe.image} className='h-50 rounded-3xl w-full cursor-pointer' onClick={() => setOverlayOpen(true)} ></img>
-                <h1 className='font-bold text-center p-3 '>{recipe.name}</h1>
+                <h1 className='font-bold text-center p-3 align-middle h-24'>{recipe.name}</h1>
 
                 <hr className='border-2 border-black mx-1'/>
 
-                <div className='flex flex-wrap pb-1 pt-1 justify-center'>
+                <div className='flex flex-wrap pt-1 justify-center'>
                     {recipe.tags.map((tag: string, index: number) => (
-                        <div key={index} className='border-2 border-black w-fit h-auto m-3 p-1 rounded-xl text-center text-xs/5'>{tag}</div>
+                        <div key={index} className='border-2 border-black w-fit h-auto m-1.5 p-1 rounded-xl text-center text-xs/5'>{tag}</div>
                     ))}
                 </div>
 
-                <div className='grid grid-cols-2 gap-4 pl-3 pr-3'>
-                    <button className="border-2 w-auto rounded-3xl bg-yellow-400 font-medium p-1">Edit</button>
-                    <button className="border-2 w-auto rounded-3xl bg-red-400 font-medium p-1">Delete</button>
+                <div className='grid grid-cols-2 gap-4 pl-3 pr-3 mt-auto'>
+                    <button className="border-2 w-auto rounded-3xl bg-yellow-400 font-medium p-1 cursor-pointer">Edit</button>
+                    <button className="border-2 w-auto rounded-3xl bg-red-400 font-medium p-1 cursor-pointer">Delete</button>
                 </div>
             </div>
             
             <Overlay isOpen={isOverlayOpen} onClose={() => setOverlayOpen(!isOverlayOpen)}>
-                <div className="w-full text-center space-y-3 pt-2">
-                    <ol className="font-bold text-3xl">Instructions</ol>
+                <div className="w-full text-left space-y-3 pt-2">
+                    <ol className="font-bold text-3xl text-center">Instructions</ol>
                     <ol className="space-y-1 text-xl font-medium">
                         {recipe.instructions.map((instruction: string, index: number) => (
                             <li key={index}>{instruction}</li>
--- PizzaShop/src/Home.tsx
+++ PizzaShop/src/Home.tsx
@@ -1,6 +1,8 @@
 import { useQuery } from "@tanstack/react-query";
 import Card from "./Card";
-import AllRecipes from "./options/AllRecipes";
+import AllRecipes from "./Recipes.ts";
+import FilterBar from "./FilterBar.tsx";
+import { useState } from "react";
 
 interface Recipe {
   id: string | number;
@@ -14,25 +16,26 @@ interface Recipe {
 function Home () {
 
     const { data, isLoading, error } = useQuery(AllRecipes())
+    const [search, setSearch] = useState<string>("")
 
     if (isLoading) return <div>Loading...</div>;
     if (error) return <div>Error loading recipes</div>;
 
     return (
         <>
-            <div className="w-full h-full bg-amber-100 ">
-                <div className='text-5xl font-bold text-center p-10'>
-                    <input type='text' className='border-2 rounded-2xl text-center p-3' placeholder="Search..."></input>
-                </div>
+            <div className="w-full min-h-screen bg-amber-100 ">
+                <FilterBar search={search} setSearch={setSearch} />
 
                 <hr className='border-2 border-black mx-16'/>
 
-                <div className='grid grid-cols-4 gap-1 p-10'>
-                    {data?.recipes?.map((recipe: Recipe) => (
-                        <Card key={recipe.id} recipe={recipe}/>
+                <div className='grid grid-cols-4 gap-y-10 p-10'>
+                    {data?.recipes?.filter((recipe: Recipe) => 
+                        recipe.name.toLowerCase().includes(search.toLowerCase())
+                    ).map((recipe: Recipe) => (
+                    
+                    <Card key={recipe.id} recipe={recipe}/>
                     ))} 
                 </div>
-            
             </div>
         </>
     )
